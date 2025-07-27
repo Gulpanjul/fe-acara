4a9a9f7cb4ae27c96a6b45378c52387fe9706429
gulpanjul <gulpa.andhikac@gmail.com>
2025-07-27 12:13:49 +0700

    feat: sp2-25-detail-and-update-event

---
+++ src/pages/admin/event/[id].tsx
@@ -0,0 +1,16 @@
+import DashboardLayout from "@/components/layouts/DashboardLayout";
+import DetailEvent from "@/components/views/Admin/DetailEvent";
+
+const AdminDetailCategoryPage = () => {
+  return (
+    <DashboardLayout
+      title="Detail Event"
+      description="Manage information for this event."
+      type="admin"
+    >
+      <DetailEvent />
+    </DashboardLayout>
+  );
+};
+
+export default AdminDetailCategoryPage;
